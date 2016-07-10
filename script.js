9db163c9425e975249ab605ccbe506d6a48eb5e9
Lauren Granger <lr.granger21@gmail.com>
2016-07-10 14:18:50 -0700

    jquery for togle of descriptons

--- script.js
+++ script.js
@@ -2,4 +2,7 @@ $( document ).ready(function() {
   $('.mail').click(function(){
     $('.email').slideToggle();
   });
+  $('.title').click(function(){
+    $('this.next.children').slideToggle();
+  });
 });
