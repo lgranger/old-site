fcd03ff1d273beada193c574d958ab329a0b3cab
Lauren Granger <lr.granger21@gmail.com>
2016-07-10 14:35:55 -0700

    moving js selctors

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ $( document ).ready(function() {
     $('.email').slideToggle();
   });
   $('.title').click(function(){
-    $('title.next(div).children').slideToggle();
+    $('.title').next(div).children.slideToggle();
   });
 });
