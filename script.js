abcce8106c19030c95f1f9e3cf04e890c6659b2e
Lauren Granger <lr.granger21@gmail.com>
2016-07-10 17:23:51 -0700

    add toggle class for selection ada

--- script.js
+++ script.js
@@ -14,5 +14,7 @@ $( document ).ready(function() {
   });
   $('.title-Ada').click(function(){
     $('.desc-Ada').slideToggle();
+    $('.title-Ada').toggleClass('title-selected');
+    $('.circle-Ada').toggleClass('circle-right-selected');
   });
 });
